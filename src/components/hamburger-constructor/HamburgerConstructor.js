06df08ecbaeefb7ae6dfeca8a841be7b6ca01c8c
Lalov Kirill <kirill.lalov@artezio-training.com>
2022-05-05 12:53:02 +0300

    feat: add new component HamburgerCreator.js

---
+++ src/components/hamburger-constructor/HamburgerConstructor.js
@@ -0,0 +1,7 @@
+import Info from "../info/Info";
+import FillingsSelector from "../fillingsSelector/FillingsSelector";
+
+export default function HamburgerConstructor() {
+    return(<>
+    </>);
+}
