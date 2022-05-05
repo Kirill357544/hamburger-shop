da785f63ed6b570b69ef9da116291665e518bb94
Lalov Kirill <kirill.lalov@artezio-training.com>
2022-05-05 15:30:25 +0300

    fix: rewrite css styles

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -6,25 +6,25 @@ export default function Info({ hamburger }) {
     return (
         <div className="col-4 mb-5">
             <dl className="border-bottom">
-                <div>
+                <div className="d-flex justify-content-evenly">
                     <dt>Size</dt>
                     <dd>{hamburger.size.name}</dd>
                 </div>
-                <div>
+                <div className="d-flex justify-content-evenly">
                     <dt>Toppings</dt>
                     <dd>{hamburger.toppings.length}</dd>
                 </div>
-                <div>
+                <div className="d-flex justify-content-evenly">
                     <dt>Stuffings</dt>
                     <dd>{hamburger.stuffings.length}</dd>
                 </div>
-                <div>
+                <div className="d-flex justify-content-evenly">
                     <dt>Calories</dt>
                     <dd>{calculateCalories(hamburger)}</dd>
                 </div>
             </dl>
             <dl className="dl-horizontal">
-                <div>
+                <div className="d-flex justify-content-evenly">
                     <dt>Price</dt>
                     <dd>{formatPrice(calculatePrice(hamburger))}</dd>
                 </div>
