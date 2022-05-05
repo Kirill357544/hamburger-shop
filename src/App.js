ec46c3472102ab3cef13c6cac391629b45f20ef9
Lalov Kirill <kirill.lalov@artezio-training.com>
2022-05-05 15:35:48 +0300

    fix: change extensions of components files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import HomePage from "./pages/home/HomePage";
 import ToppingsPage from "./pages/toppings/ToppingsPage";
-import StuffingsPage from "./pages/stuffings/StuffingsPage.js";
+import StuffingsPage from "./pages/stuffings/StuffingsPage";
 import HistoryPage from "./pages/history/HistoryPage";
 import OrderPage from "./pages/order/OrderPage";
 import OrderButton from "./components/header/OrderButton";
