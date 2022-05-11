b5239095ff87150c5b88a3ba7464db379af9de11
Lalov Kirill <kirill.lalov@artezio-training.com>
2022-05-11 09:04:28 +0300

    fix: rewrite flex to grid

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -5,28 +5,28 @@ import calculateCalories from "../../calculate/calculateCalories";
 export default function Info({ hamburger }) {
     return (
         <div className="col-4 mb-5">
-            <dl className="border-bottom">
-                <div className="d-flex justify-content-evenly">
-                    <dt>Size</dt>
-                    <dd>{hamburger.size.name}</dd>
+            <dl className="container border-bottom">
+                <div className="row">
+                    <dt className="col text-end">Size</dt>
+                    <dd className="col">{hamburger.size.name}</dd>
                 </div>
-                <div className="d-flex justify-content-evenly">
-                    <dt>Toppings</dt>
-                    <dd>{hamburger.toppings.length}</dd>
+                <div className="row">
+                    <dt className="col text-end">Toppings</dt>
+                    <dd className="col">{hamburger.toppings.length}</dd>
                 </div>
-                <div className="d-flex justify-content-evenly">
-                    <dt>Stuffings</dt>
-                    <dd>{hamburger.stuffings.length}</dd>
+                <div className="row">
+                    <dt className="col text-end">Stuffings</dt>
+                    <dd className="col">{hamburger.stuffings.length}</dd>
                 </div>
-                <div className="d-flex justify-content-evenly">
-                    <dt>Calories</dt>
-                    <dd>{calculateCalories(hamburger)}</dd>
+                <div className="row">
+                    <dt className="col text-end">Calories</dt>
+                    <dd className="col">{calculateCalories(hamburger)}</dd>
                 </div>
             </dl>
-            <dl className="dl-horizontal">
-                <div className="d-flex justify-content-evenly">
-                    <dt>Price</dt>
-                    <dd>
+            <dl className="container">
+                <div className="row">
+                    <dt className="col text-end">Price</dt>
+                    <dd className="col">
                         <Price price={calculatePrice(hamburger)} />
                     </dd>
                 </div>
